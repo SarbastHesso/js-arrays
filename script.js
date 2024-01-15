ad3b44bf4ffa8653a834e4f304a757fa166b9d96
Sarbast Hesso <sarbast.hesso@gmail.com>
2024-01-15 14:24:28 +0100

    finished the assignment

---
+++ script.js
@@ -0,0 +1,56 @@
+const numbers = [2, 6, 12, 7, 22, 35];
+
+
+// 1. Create a variable where you store the first element of the `numbers` array. Log it to the console.
+// const firstEl = numbers[0];
+// console.log(firstEl);
+
+
+// 2. Create a variable where you store the last element of the `numbers` array. Log it to the console.
+// const lastEl = numbers[numbers.length - 1];
+// console.log(lastEl);
+
+
+// 3. Create a variable where you store the fourth element of the `numbers` array. Log it to the console.
+// const fourthEl = numbers[3];
+// console.log(fourthEl);
+
+
+// 4. Store the second and the fifth element of the `numbers` array in variables. Create a new array and populate it with those two variables. Log it to the console.
+// const secondEl = numbers[1];
+// const fifthEl = numbers[4];
+// const newArray = [secondEl, fifthEl];
+// console.log(newArray);
+
+
+// 5. What is the length of the `numbers` array? Use the correct array property to save that number to a variable and log it to the console.
+// const arrayLength = numbers.length;
+// console.log(arrayLength);
+
+
+// 6. Add an arbitrary number to the end of the `numbers` array with the correct array method. This array method returns a value. Save that to a variable and log it to the console. What does this number represent?
+// const returnedValueAfterPush = numbers.push(99);
+// console.log(returnedValueAfterPush);
+// The returned number represent the numbers of elements in the Array after adding the new element.
+
+
+// 7. Add an arbitrary number to the start of the `numbers` array using the correct array method. This method also returns a value. Log it to the console and reflect on what this value means.
+// const returnedValueAfterUnshift = numbers.unshift(77);
+// console.log(returnedValueAfterUnshift);
+// The returned number represent the numbers of elements in the Array after adding the new element.
+
+
+// 8. By now the `numbers` array should have been modified a couple of times. Let's shrink it again. Remove the last element from the array with an array method. This method should return a value as well. Log it to the console as see what it is.
+// const returnedValueAfterPop = numbers.pop();
+// console.log(returnedValueAfterPop);
+// The returned number represent the element that have been deleted from the Array.
+
+
+// 9. Do the same as the last one, but remove it from the beginning instead. Log the return value to the console.
+// const returnedValueAfterShift = numbers.shift();
+// The returned number represent the element that have been deleted from the Array.
+
+
+// 10. If we have done the last couple of exercises correctly, we should have the same content as the default array. Log it out the console and see for yourself. 
+// console.log(numbers);
+
